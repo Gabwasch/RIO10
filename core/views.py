b02feb2b20be472921e17d1a316fb53fecd55fd8
Kelvin Costa Da Silveira <kelvin.silveira@andrademaia.com>
2024-01-23 18:16:42 -0300

    pagina de cadastro

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# core/views.py
+from django.shortcuts import render, redirect
+from .forms import CadastroForm
 
-# Create your views here.
+def cadastro_view(request):
+    if request.method == 'POST':
+        form = CadastroForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()  # Isso salva os dados no banco de dados
+            except Exception as e:
+                print(f"Erro ao salvar no banco de dados: {e}")
+            return redirect('cadastro')  # Pode redirecionar para outra página após o cadastro
+    else:
+        form = CadastroForm()
+
+    return render(request, 'cadastro.html', {'form': form})
